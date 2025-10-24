5461b3aaaa6b17724094b3d683cd8c1c8d12e94f
Maksym <maksymtautkevychius@gmail.com>
2025-10-24 21:31:56 +0200

    New Project Structure

---
+++ apps/backend/src/Agents/mainAgent.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv 
+import os
+
+OpenAPI = os.getenv('OpenAPI')
+os.environ["OPEN_API_KEY"]= OpenAPI
+openai_gpt5 = 'gpt-5'
+openai_gpt4_1='gpt-4.1'
+openai_gpt5mini='gpt-5-mini'
+alibaba_qwen_3max='qwen3-max'
+
+def personalInfoAgent():
+    return
+
+def mainAgent():
+    return
+
+def helperAgent():
+    return
+
+def summaryAgent():
+    return
